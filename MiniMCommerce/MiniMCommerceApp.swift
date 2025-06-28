71a65232d38639f0304bbb0c85fbf55d876a03dd
Denidu Gamage <denidugamage21@gmail.com>
2025-06-28 09:38:20 +0530

    Initial Commit

---
+++ MiniMCommerce/MiniMCommerceApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiniMCommerceApp.swift
+//  MiniMCommerce
+//
+//  Created by Denidu Gamage on 2025-06-28.
+//
+
+import SwiftUI
+
+@main
+struct MiniMCommerceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MiniMCommerceTests/MiniMCommerceTests.swift
@@ -0,0 +1,17 @@
+//
+//  MiniMCommerceTests.swift
+//  MiniMCommerceTests
+//
+//  Created by Denidu Gamage on 2025-06-28.
+//
+
+import Testing
+@testable import MiniMCommerce
+
+struct MiniMCommerceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
